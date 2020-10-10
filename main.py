6b033960c6ebc6cd89ab5d24e806098fb0e99421
Jason <jgodmere808@gmail.com>
2020-10-10 06:15:12 -0500

    added sensor reading data (cpu temps, battery, fans)

--- main.py
+++ main.py
@@ -46,9 +46,6 @@ print("CPU Usage Per Core:")
 for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
     print(f"Core {i}: {percentage}%")
 print(f"Total CPU Usage: {psutil.cpu_percent()}%")
-# CPU temps
-if uname.system != "Windows":
-	print("CPU Temp:", psutil.sensors_temperatures())
 
 # Memory Information
 print("="*40, "Memory Information", "="*40)
